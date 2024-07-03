8d59350f1a310e980569c40b9df9a9fc4d92ebf0
rosmis <rosmis123@gmail.com>
2024-07-03 12:09:53 +0200

    update on greet + test

--- src/AnalyseurPalindrome.ts
+++ src/AnalyseurPalindrome.ts
@@ -1,7 +1,24 @@
 export class AnalyseurPalindrome {
+    static Analyser(chaîne: string): string {
+        let output = "";
 
-    static Analyser(chaîne: string) : string {
-        let miroir = chaîne.split('').reverse().join('')
-        return chaîne == miroir ? miroir + '\n' + 'Bien dit !' : miroir
+        output += this.Greet(output);
+        output += this.Reverse(chaîne);
+
+        return output;
+    }
+
+    static Greet(chaine: string): string {
+        return (chaine += "Bonjour\n");
+    }
+
+    static Reverse(chaine: string): string {
+        const reversed = chaine.split("").reverse().join("");
+
+        if (chaine == reversed) {
+            return chaine + "\n" + "Bien dit !";
+        }
+
+        return reversed;
     }
 }
--- test/ohce.test.ts
+++ test/ohce.test.ts
@@ -1,20 +1,30 @@
-import {AnalyseurPalindrome} from "../src/AnalyseurPalindrome";
+import { AnalyseurPalindrome } from "../src/AnalyseurPalindrome";
 
-describe('ETANT DONNE une chaîne QUAND on l\'analyse ALORS on obtient son miroir',
-    () => {
-        test.each(["test", "ynov"])
-        ('Cas %s', (chaîne) => {
-            let résultat = AnalyseurPalindrome.Analyser(chaîne)
-            let expected = chaîne.split('').reverse().join('')
-            expect(résultat).toBe(expected)
-        })
+describe("ETANT DONNE une chaîne QUAND on l'analyse ALORS on obtient son miroir", () => {
+    test.each(["test", "ynov"])("Cas %s", (chaîne) => {
+        let résultat = AnalyseurPalindrome.Analyser(chaîne);
+        let expected = chaîne.split("").reverse().join("");
+        expect(résultat).toContain(expected);
+    });
+});
+
+describe("ETANT DONNE un palindrome QUAND on l'analyse ALORS il ressort suivi de 'Bien dit !'", () => {
+    test.each(["radar", "kayak"])("Cas %s", (chaîne) => {
+        let résultat = AnalyseurPalindrome.Analyser(chaîne);
+        expect(résultat).toContain(chaîne + "\n" + "Bien dit !");
+    });
+});
+
+describe("ETANT DONNE une chaine QUAND on l'analyse ALORS on obtient son miroir précédé de bonjour", () => {
+    test("Cas bonjour", () => {
+        const resultat = AnalyseurPalindrome.Analyser("test");
+
+        expect(resultat).toContain("Bonjour");
     });
 
-describe('ETANT DONNE un palindrome QUAND on l\'analyse ALORS il ressort suivi de \'Bien dit !\'',
-    () => {
-        test.each(["radar", "kayak"])
-        ('Cas %s', (chaîne) => {
-            let résultat = AnalyseurPalindrome.Analyser(chaîne)
-            expect(résultat).toBe(chaîne + '\n' + "Bien dit !")
-        })
-    });
+    test('cas bonjour est bien avant le miroir', () => {
+        const resultat = AnalyseurPalindrome.Analyser("test");
+
+        expect(resultat).toContain("Bonjour\ntset");
+    })
+});
